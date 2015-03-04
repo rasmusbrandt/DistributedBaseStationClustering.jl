f3bb1fa91038fab72f9a9c39dd51217069307f6f
Rasmus Brandt <rasmusb85@gmail.com>
2015-03-04 19:08:51 +0100

    Generalize a little bit.

--- src/assignment/ExhaustiveSearchClustering.jl
+++ src/assignment/ExhaustiveSearchClustering.jl
@@ -1,7 +1,5 @@
 function ExhaustiveSearchClustering(channel, network)
     I = get_no_BSs(network); K = get_no_MSs(network)
-    Ps = get_transmit_powers(network)
-    sigma2s = get_receiver_noise_powers(network)
     ds = get_no_streams(network)
 
     # Perform cell selection
@@ -16,21 +14,7 @@ function ExhaustiveSearchClustering(channel, network)
         # Check that IA is feasible for this cluster structure
         if is_IA_feasible(partition, channel.Ns, channel.Ms, ds, temp_assignment)
             # Calculate rates
-            for block in partition.blocks
-                intercluster_interferers = setdiff(1:I, block.elements)
-                for i in block.elements
-                    for k in served_MS_ids(i, temp_assignment)
-                        desired_power = channel.large_scale_fading_factor[k,i]^2*Ps[i]
-                        int_noise_power = sigma2s[k]
-                        for j in intercluster_interferers
-                            int_noise_power += channel.large_scale_fading_factor[k,j]^2*Ps[j]
-                        end
-                        rho = 1 + desired_power/int_noise_power
-
-                        rates[k] = 0.5log(1 + 2rho)
-                    end
-                end
-            end
+            rates = longterm_cluster_IA_rates(channel, network, partition)
 
             sum_rate = sum(rates)
             if sum_rate > best_sum_rate
--- src/assignment/assignment.jl
+++ src/assignment/assignment.jl
@@ -12,6 +12,30 @@ function partition_to_cluster_assignment_matrix(partition, K, I, assignment)
     return cluster_assignment_matrix
 end
 
+function longterm_cluster_IA_rates(channel, network, partition)
+    I = get_no_BSs(network); K = get_no_MSs(network)
+    Ps = get_transmit_powers(network)
+    sigma2s = get_receiver_noise_powers(network)
+
+    rates = zeros(Float64, K)
+
+    for block in partition.blocks
+        intercluster_interferers = setdiff(1:I, block.elements)
+        for i in block.elements; for k in served_MS_ids(i, temp_assignment)
+            desired_power = channel.large_scale_fading_factor[k,i]^2*Ps[i]
+            int_noise_power = sigma2s[k]
+            for j in intercluster_interferers
+                int_noise_power += channel.large_scale_fading_factor[k,j]^2*Ps[j]
+            end
+            rho = desired_power/int_noise_power
+
+            rates[k] = 0.5log(1 + 2rho)
+        end; end
+    end
+
+    return rates
+end
+
 include("BnBClustering.jl")
 include("Chen2014_LinearObjClustering.jl")
 include("ExhaustiveSearchClustering.jl")
