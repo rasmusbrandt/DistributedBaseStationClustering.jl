f5ac9ffab1395c4c8b54745bf8bfdd9815932570
Rasmus Brandt <rasmusb85@gmail.com>
2015-03-05 15:02:43 +0100

    Refactored set partition enumeration using Algorithm H from TAoCP.
    Signed-off-by: Rasmus Brandt <rasmusb85@gmail.com>

--- src/utils/partitions.jl
+++ src/utils/partitions.jl
@@ -1,78 +1,117 @@
+##########################################################################
+# Partitions
+
+# A block is just a set of integers.
 immutable Block
     elements::Vector{Int}
 end
 Block() = Block([])
-Block(e::Int) = Block([e])
-Block(e1::Int, e2::Int) = Block([e1, e2])
-Block(elements::Vector{Int}, extra_element::Int) = Block(vcat(elements, extra_element))
 getindex(b::Block, idx) = b.elements[idx]
-Base.show(io::IO, b::Block) = showcompact(io, b.elements)
 Base.push!(b::Block, e::Int) = push!(b.elements, e)
+Base.show(io::IO, b::Block) = showcompact(io, b.elements)
 Base.length(b::Block) = length(b.elements)
 
+# The Partition type describes a partition of integers into blocks.
 immutable Partition
     blocks::Vector{Block}
 end
-Partition() = Partition([])
-Partition(b::Block) = Partition([b])
-Partition(b1::Block, b2::Block) = Partition([b1, b2])
-Partition(blocks::Vector{Block}, extra_block::Block) = Partition(vcat(blocks, extra_block))
-getindex(p::Partition, idx) = p.blocks[idx]
 Base.show(io::IO, p::Partition) = showcompact(io, p.blocks)
 Base.length(p::Partition) = length(p.blocks)
 
-add_element_to_block!(p::Partition, e::Int, block_idx::Int) =
-    push!(p[block_idx], e)
+# Create partition from restricted growth string
+function Partition(rgs::Vector)
+    no_blocks = maximum(rgs) + 1
+    blocks = [ Block() for n = 1:no_blocks ]
 
-function all_twoblock_partitions(elements::Block)
-    N = length(elements)
-
-    # No result if less than two elements
-    if N < 2
-        return Partition[]
+    for l = 1:length(rgs)
+        push!(blocks[rgs[l] + 1], l)
     end
 
-    # Generate all two-block partitions
-    partitions = Partition[]
-    for pattern = 1:((1 << (N-1)) - 1)
-        # Create new partition. We specifically put the first element in the
-        # first subset, to avoid ordering problems.
-        partition = Partition(Block(elements[1]), Block())
+    return Partition(blocks)
+end
 
-        # Distribute the remaining elements into the two blocks
-        for idx = 2:N
-            add_element_to_block!(partition, elements[idx], ((pattern >> (idx - 2)) & 1) + 1)
+# PartitionIterator gives all partitions of the set 1:n. This iterator is
+# based on Algorithm H from TAoCP 7.2.1.5.
+immutable PartitionIterator
+    n::Int
+
+    PartitionIterator(n) = n >= 2 ? new(n) : error("only applicable for n >= 2.")
+end
+
+type PartitionIteratorState
+    a::Vector{Int}
+    b::Vector{Int}
+    m::Int
+    terminate::Bool
+end
+PartitionIteratorState(n) = PartitionIteratorState(zeros(n), ones(n - 1), 1, false)
+
+Base.start(p::PartitionIterator) = PartitionIteratorState(p.n) # H1
+Base.done(p::PartitionIterator, s) = s.terminate
+function Base.next(p::PartitionIterator, s)
+    # H2
+    current_partition = Partition(s.a)
+
+    if s.a[p.n] < s.m
+        # H3
+        s.a[p.n] += 1
+    else
+        # H4
+        j = p.n - 1
+        while s.a[j] == s.b[j]
+            j -= 1
         end
 
-        # Store this partition in the list of all partitions
-        push!(partitions, partition)
-    end
+        # H5
+        if j == 1
+            s.terminate = true
+        else
+            s.a[j] += 1
 
-    return partitions
-end
-
-function all_partitions(elements)
-    partitions = Partition[]
-    all_partitions(partitions, Block[], Block(elements))
-    return partitions
-end
-
-function all_partitions(partitions::Vector{Partition}, fixed_blocks::Vector{Block}, suffix_elements::Block)
-    # The trivial partition is adding the suffix elements as one block
-    # after all fixed blocks.
-    push!(partitions, Partition(fixed_blocks, suffix_elements))
-
-    # Get all two-block partitions of the suffix elements, and sub-divide
-    # them recursively.
-    suffix_partitions = all_twoblock_partitions(suffix_elements)
-    sub_partitions_return = Partition[]
-    for suffix_partition in suffix_partitions
-        sub_partitions = all_partitions(partitions, push!(copy(fixed_blocks), suffix_partition[1]), suffix_partition[2])
-        for sub_partition in sub_partitions
-            push!(partitions, sub_partition)
-            push!(sub_partitions_return, sub_partition)
+            # H6
+            s.m = s.b[j] + (s.a[j] == s.b[j] ? 1 : 0)
+            j += 1
+            while j < p.n
+                s.a[j] = 0
+                s.b[j] = s.m
+                j += 1
+            end
+            s.a[p.n] = 0
         end
     end
 
-    return sub_partitions_return
+    # H2 cont'd.
+    return current_partition, s
 end
+
+##########################################################################
+# Stirling and Bell numbers
+
+# Stirling2NumberCoefficientIterator gives all coefficients that when summed
+# result in a Stirling number of the second kind. Note that the coefficients
+# may be real numbers, but when summed are integers. The coefficients are
+# thus calculated as floating point numbers.
+immutable Stirling2NumberCoefficientIterator
+    n::Int
+    k::Int
+    k_fac::Int
+end
+Stirling2NumberCoefficientIterator(n, k) = Stirling2NumberCoefficientIterator(n, k, factorial(k))
+Base.start(s::Stirling2NumberCoefficientIterator) = 0 # state: k
+Base.done(s::Stirling2NumberCoefficientIterator, j) = (j > s.k)
+Base.next(s::Stirling2NumberCoefficientIterator, j) = ((iseven(s.k - j) ? 1. : -1.)*(binomial(s.k, j)*j^s.n/s.k_fac), j + 1)
+
+# Stirling2NumberIterator gives all Stirling numbers of the second kind
+# up to n. The Stirling numbers are integers, but the Stirling number
+# coefficients are not. We therefore round the sum of the coefficients,
+# in order to end up with an integer.
+immutable Stirling2NumberIterator
+    n::Int
+end
+Base.start(s::Stirling2NumberIterator) = 0 # state: k
+Base.done(s::Stirling2NumberIterator, k) = (k > s.n)
+Base.next(s::Stirling2NumberIterator, k) = (iround(sum(Stirling2NumberCoefficientIterator(s.n, k))), k + 1)
+
+# The Bell number can we calculated as the sum of all
+# Stirling numbers of the second kind.
+bell_number(n) = sum(Stirling2NumberIterator(n))
