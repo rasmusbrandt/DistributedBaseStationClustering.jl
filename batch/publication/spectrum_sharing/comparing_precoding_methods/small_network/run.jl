5ddbd32b76c4cc2e93f0aed8ed9c9b8fda8ad1fc
Rasmus Brandt <rasmusb85@gmail.com>
2015-04-13 17:57:58 +0200

    Add small network publication batch runs.

---
+++ batch/publication/spectrum_sharing/comparing_precoding_methods/small_network/run.jl
@@ -0,0 +1,43 @@
+#!/usr/bin/env julia
+
+SRAND_SEED = 9825242
+
+include(joinpath(dirname(@__FILE__), "../../../../../src/IAClustering.jl"))
+using IAClustering, CoordinatedPrecoding
+using HDF5, JLD
+
+include(joinpath(dirname(@__FILE__), "../../../simulation_params.jl"))
+include(joinpath(dirname(@__FILE__), "../../../simulation_params-small_network.jl"))
+include(joinpath(dirname(@__FILE__), "../../../simulation_params-SNR.jl"))
+include(joinpath(dirname(@__FILE__), "../../../simulation_params-precoding_methods.jl"))
+include(joinpath(dirname(@__FILE__), "../../../plot_params-precoding_methods.jl"))
+
+##########################################################################
+# Plot setup
+plot_params["axes"][:xlabel] = "MS speed [km/h]"
+
+##########################################################################
+# Simulation (both MinWLI and WMMSE)
+start_time = strftime("%Y%m%dT%H%M%S", time())
+
+srand(SRAND_SEED)
+simulation_params["simulation_name"] = "precoding_methods-small_network-precoding_$(start_time)"
+network =
+    setup_random_large_scale_network(simulation_params["I"],
+        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
+        no_streams=simulation_params["d"],
+        geography_size=simulation_params["geography_size"],
+        MS_serving_BS_distance=simulation_params["MS_serving_BS_distance"])
+raw_precoding_results, raw_assignment_results =
+    simulate(network, simulation_params, loop_over=:precoding_methods)
+
+println("-- Saving $(simulation_params["simulation_name"]) results")
+save("$(simulation_params["simulation_name"]).jld",
+     "simulation_params", clean_simulation_params_for_jld(simulation_params),
+     "raw_precoding_results", raw_precoding_results,
+     "raw_assignment_results", raw_assignment_results)
+
+##########################################################################
+# Generic plots
+processed_results = postprocess(raw_precoding_results, simulation_params, plot_params)
+plot(processed_results, simulation_params, plot_params)
---
+++ batch/publication/spectrum_sharing/sweeping_SNR/small_network/run.jl
@@ -0,0 +1,76 @@
+#!/usr/bin/env julia
+
+SRAND_SEED = 927272
+
+include(joinpath(dirname(@__FILE__), "../../../../../src/IAClustering.jl"))
+using IAClustering, CoordinatedPrecoding
+using HDF5, JLD
+
+include(joinpath(dirname(@__FILE__), "../../../simulation_params.jl"))
+include(joinpath(dirname(@__FILE__), "../../../simulation_params-small_network.jl"))
+include(joinpath(dirname(@__FILE__), "../../../simulation_params-SNR.jl"))
+include(joinpath(dirname(@__FILE__), "../../../plot_params-assignment_methods.jl"))
+
+##########################################################################
+# Plot setup
+for p in (plot_params_instantaneous_coord_sumrate, plot_params_instantaneous_noncoord_sumrate, plot_params_longterm_sumrate, plot_params_longterm_no_utility_calculations, plot_params_longterm_no_clusters)
+    p["axes"][:xlabel] = "Signal-to-noise ratio [dB]"
+end
+
+##########################################################################
+# Simulation (both MinWLI and WMMSE)
+start_time = strftime("%Y%m%dT%H%M%S", time())
+
+srand(SRAND_SEED)
+simulation_params["simulation_name"] = "SNR-small_network-assignment-MinWLI_$(start_time)"
+simulation_params["precoding_methods"] = [ RobustIntraclusterLeakageMinimization ]
+network =
+    setup_random_large_scale_network(simulation_params["I"],
+        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
+        no_streams=simulation_params["d"],
+        geography_size=simulation_params["geography_size"],
+        MS_serving_BS_distance=simulation_params["MS_serving_BS_distance"])
+raw_precoding_results1, raw_assignment_results1 =
+    simulate(network, simulation_params, loop_over=:assignment_methods)
+
+processed_results = postprocess(raw_precoding_results1, simulation_params, plot_params_instantaneous_coord_sumrate)
+plot(processed_results, simulation_params, plot_params_instantaneous_coord_sumrate)
+processed_results = postprocess(raw_precoding_results1, simulation_params, plot_params_instantaneous_noncoord_sumrate)
+plot(processed_results, simulation_params, plot_params_instantaneous_noncoord_sumrate)
+
+println("-- Saving $(simulation_params["simulation_name"]) results")
+save("$(simulation_params["simulation_name"]).jld",
+     "simulation_params", clean_simulation_params_for_jld(simulation_params),
+     "raw_precoding_results", raw_precoding_results1,
+     "raw_assignment_results", raw_assignment_results1)
+
+srand(SRAND_SEED)
+simulation_params["simulation_name"] = "SNR-small_network-assignment-WMMSE_$(start_time)"
+simulation_params["precoding_methods"] = [ RobustIntraclusterWMMSE ]
+network =
+    setup_random_large_scale_network(simulation_params["I"],
+        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
+        no_streams=simulation_params["d"],
+        geography_size=simulation_params["geography_size"],
+        MS_serving_BS_distance=simulation_params["MS_serving_BS_distance"])
+raw_precoding_results2, raw_assignment_results2 =
+    simulate(network, simulation_params, loop_over=:assignment_methods)
+
+processed_results = postprocess(raw_precoding_results2, simulation_params, plot_params_instantaneous_coord_sumrate)
+plot(processed_results, simulation_params, plot_params_instantaneous_coord_sumrate)
+processed_results = postprocess(raw_precoding_results2, simulation_params, plot_params_instantaneous_noncoord_sumrate)
+plot(processed_results, simulation_params, plot_params_instantaneous_noncoord_sumrate)
+
+println("-- Saving $(simulation_params["simulation_name"]) results")
+save("$(simulation_params["simulation_name"]).jld",
+     "simulation_params", clean_simulation_params_for_jld(simulation_params),
+     "raw_precoding_results", raw_precoding_results2,
+     "raw_assignment_results", raw_assignment_results2)
+
+##########################################################################
+# Generic plots
+simulation_params["simulation_name"] = "SNR-small_network-assignment_$(start_time)"
+for p in (plot_params_longterm_sumrate, plot_params_longterm_no_utility_calculations, plot_params_longterm_no_clusters)
+    processed_results = postprocess(raw_assignment_results1, simulation_params, p)
+    plot(processed_results, simulation_params, p)
+end
