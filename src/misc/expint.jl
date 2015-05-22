2632eb669e8c84c5a97c990fe352a65386d58924
Rasmus Brandt <rasmusb85@gmail.com>
2015-05-22 15:23:40 +0200

    expint: move polynomial definition outside function.

--- src/misc/expint.jl
+++ src/misc/expint.jl
@@ -1,22 +1,21 @@
+const expint_poly = Polynomials.Poly([
+     8.267661952366478e+00,
+    -7.773807325735529e-01,
+    -3.012432892762715e-01,
+    -7.811863559248197e-02,
+    -1.019573529845792e-02,
+    -6.973790859534190e-04,
+    -2.569498322115933e-05,
+    -4.819538452140960e-07,
+    -3.602693626336023e-09
+])
+
 function expint(x::Float64)
     y = 0.
     eps1 = eps()
 
-    p = Polynomials.Poly([
-         8.267661952366478e+00,
-        -7.773807325735529e-01,
-        -3.012432892762715e-01,
-        -7.811863559248197e-02,
-        -1.019573529845792e-02,
-        -6.973790859534190e-04,
-        -2.569498322115933e-05,
-        -4.819538452140960e-07,
-        -3.602693626336023e-09
-    ])
-    polyv = Polynomials.polyval(p, x)
-
-    # series expansion
-    if polyv >= 0.
+    if Polynomials.polyval(expint_poly, x) >= 0.
+        # series expansion
         y = -0.57721566490153286061 - log(x)
         j = 1
         pterm = x
@@ -28,10 +27,8 @@ function expint(x::Float64)
             pterm = -x*pterm/j
             term = pterm/j
         end
-    end
-
-    # continued fraction
-    if polyv < 0.
+    else
+        # continued fraction
         n = 1
         am2 = 0.
         bm2 = 1.
