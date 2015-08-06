fcd3162a771ecd6aa5ff62286a8bec2b4fdd6a1b
Rasmus Brandt <rasmusb85@gmail.com>
2015-08-06 15:42:07 +0200

    Conference scripts are not relevant anymore, with the new utility model.
    See the `asilomar_model` tag instead.

--- batch/conference/simulation_params-large_network.jl
+++
@@ -1,8 +0,0 @@
-simulation_params["I"] = 16
-simulation_params["Kc"] = 1
-simulation_params["N"] = 2
-simulation_params["M"] = 4
-simulation_params["d"] = 1
-
-l = sqrt(simulation_params["I"]/BS_density)
-simulation_params["geography_size"] = (l, l)
