fdf7ed46a00d308de9066a87edc73a76b0e3501a
Rasmus Brandt <rasmusb85@gmail.com>
2015-08-12 12:20:08 +0200

    Avoid cycling issue for beta = 0, by fudging the studied range a bit.

--- batch/general_investigation/simulation_params-beta.jl
+++ batch/general_investigation/simulation_params-beta.jl
@@ -1,4 +1,4 @@
-simulation_params["independent_variable"] = ((n, v) -> set_aux_network_param!(n, v, "beta_network_sdma"), 0:0.2:1)
+simulation_params["independent_variable"] = ((n, v) -> set_aux_network_param!(n, v, "beta_network_sdma"), [1e-5, 0.2:0.2:1])
 simulation_params["aux_independent_variables"] = [
     ((n, v) -> set_average_SNRs_dB!(n, v), [SNR_dB]),
     ((n, v) -> set_aux_network_param!(n, v, "num_coherence_symbols"), [num_coherence_symbols]),
