7a8e4bc70ee7002394fe22fc00b8b4f5f6161852
Rasmus Brandt <rasmusb85@gmail.com>
2015-08-20 15:40:50 +0200

    Only every 3rd integer SNR. Remove old journal sims.

--- batch/journal/simulation_params-SNR.jl
+++ batch/journal/simulation_params-SNR.jl
@@ -1,4 +1,4 @@
-simulation_params["independent_variable"] = (set_average_SNRs_dB!, -10:50)
+simulation_params["independent_variable"] = (set_average_SNRs_dB!, -10:3:50)
 simulation_params["aux_independent_variables"] = [
     ((n, v) -> set_aux_assignment_param!(n, v, "CoalitionFormationClustering:search_budget"), [5, 100_000]),
     ((n, v) -> set_aux_network_param!(n, v, "num_coherence_symbols"), [num_coherence_symbols, num_coherence_symbols]),
--- batch/journal/sweeping_beta/run.sh
+++ batch/journal/sweeping_beta/run.sh
@@ -2,6 +2,9 @@
 
 seeds=(26717 93365 50261 21138 66869 27791 12175 69683 410 57971)
 
+rm -r previous; mkdir previous
+mv *.eps *.jld *.pdf *.log previous
+
 for seed in ${seeds[*]}
 do
     echo "Performing with seed $seed..."
