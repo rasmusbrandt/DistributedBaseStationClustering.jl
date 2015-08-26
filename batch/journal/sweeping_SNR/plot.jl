2791e0a341517a450323dd1cb5dd61a69155c9bb
Rasmus Brandt <rasmusb85@gmail.com>
2015-08-26 15:13:48 +0200

    Edit SNR plot in journal plot.

--- batch/journal/sweeping_SNR/plot.jl
+++ batch/journal/sweeping_SNR/plot.jl
@@ -22,7 +22,7 @@ PyPlot.rc("text.latex", preamble="\\usepackage{amsmath}")
 PyPlot.rc("axes", linewidth=0.5, labelsize=8)
 PyPlot.rc("xtick", labelsize=8)
 PyPlot.rc("ytick", labelsize=8)
-PyPlot.rc("legend", fancybox=true, fontsize=6)
+PyPlot.rc("legend", fancybox=true, fontsize=7)
 PyPlot.rc("figure", figsize=(3.50,2.16), dpi=125)
 
 # Legend creation helper
@@ -41,63 +41,33 @@ yvals_precoding(method, plot_val) = data["processed_precoding_results"][mean_idx
 # longterm-sumrate
 plot_name = "longterm-sumrate"; plot_val = "throughputs"
 fig = PyPlot.figure()
-ax = fig[:add_axes]((0.11,0.15,0.95-0.11,0.95-0.15))
+ax = fig[:add_axes]((0.11,0.15,0.98-0.11,0.96-0.15))
 
+lines = Any[]
 for method in [:BranchAndBoundClustering, :CoalitionFormationClustering_AttachOrSupplant, :Peters2012_Heuristic, :NoClustering, :GrandCoalitionClustering]
     if method == :CoalitionFormationClustering_AttachOrSupplant
-        ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,2],
+        line = ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,2],
             color=colours_assignment[method],
             linestyle="-",
             marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=latexstring("$(labels_assignment[method]) (\$b_i = \\infty\$)"))
-        ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,1],
+            label=latexstring("Coalition form. (\$b_i = \\infty\$)"))
+        line = ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,1],
             color=colours_assignment[method],
             linestyle="--",
             marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=latexstring("$(labels_assignment[method]) (\$b_i = 5\$)"))
+            label=latexstring("Coalition form. (\$b_i = 5\$)"))
     else
-        ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,1],
+        line = ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,1],
             color=colours_assignment[method],
             linestyle=linestyles_assignment[method],
             marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
             label=labels_assignment[method])
     end
+    push!(lines, line[1])
 end
 ax[:set_ylim]([-1, 60])
 ax[:set_xlabel]("Signal-to-noise ratio [dB]")
-ax[:set_ylabel]("Long-term sum throughput [bits/s/Hz]")
-show_legend!(ax, "upper left")
-fig[:savefig]("$(sim_name)_$(plot_name).eps")
-
-##########################################################################
-# longterm-sumrate_attachonly
-plot_name = "longterm-sumrate_attachonly"; plot_val = "throughputs"
-fig = PyPlot.figure()
-ax = fig[:add_axes]((0.11,0.15,0.95-0.11,0.95-0.15))
-
-for method in [:BranchAndBoundClustering, :CoalitionFormationClustering_AttachOrSupplant, :CoalitionFormationClustering_Attach, :NoClustering]
-    if method == :CoalitionFormationClustering_AttachOrSupplant || method == :CoalitionFormationClustering_Attach
-        ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,2],
-            color=colours_assignment[method],
-            linestyle="-",
-            marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=latexstring("$(labels_assignment[method]) (\$b_i = \\infty\$)"))
-        ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,1],
-            color=colours_assignment[method],
-            linestyle="--",
-            marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=latexstring("$(labels_assignment[method]) (\$b_i = 5\$)"))
-    else
-        ax[:plot](xvals, yvals_assignment(string(method), plot_val)[:,1],
-            color=colours_assignment[method],
-            linestyle=linestyles_assignment[method],
-            marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=labels_assignment[method])
-    end
-end
-ax[:set_ylim]([-1, 60])
-ax[:set_xlabel]("Signal-to-noise ratio [dB]")
-ax[:set_ylabel]("Long-term sum throughput [bits/s/Hz]")
+ax[:set_ylabel]("IIA sum throughput [bits/s/Hz]")
 show_legend!(ax, "upper left")
 fig[:savefig]("$(sim_name)_$(plot_name).eps")
 
@@ -105,28 +75,19 @@ fig[:savefig]("$(sim_name)_$(plot_name).eps")
 # longterm-num_searches
 plot_name = "longterm-num_searches"; plot_val = "num_searches"
 fig = PyPlot.figure()
-ax = fig[:add_axes]((0.11,0.15,0.95-0.11,0.95-0.15))
+ax = fig[:add_axes]((0.09,0.15,0.98-0.09,0.96-0.15))
 
-for method in [:CoalitionFormationClustering_AttachOrSupplant, :CoalitionFormationClustering_Attach]
-    if method == :CoalitionFormationClustering_AttachOrSupplant || method == :CoalitionFormationClustering_Attach
-        ax[:plot](xvals, (1/I)*yvals_assignment(string(method), plot_val)[:,2],
-            color=colours_assignment[method],
-            linestyle="-",
-            marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=latexstring("$(labels_assignment[method]) (\$b_i = \\infty\$)"))
-        ax[:plot](xvals, (1/I)*yvals_assignment(string(method), plot_val)[:,1],
-            color=colours_assignment[method],
-            linestyle="--",
-            marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=latexstring("$(labels_assignment[method]) (\$b_i = 5\$)"))
-    else
-        ax[:plot](xvals, (1/I)*yvals_assignment(string(method), plot_val)[:,1],
-            color=colours_assignment[method],
-            linestyle=linestyles_assignment[method],
-            marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
-            label=labels_assignment[method])
-    end
-end
+method = :CoalitionFormationClustering_AttachOrSupplant
+ax[:plot](xvals, (1/I)*yvals_assignment(string(method), plot_val)[:,2],
+    color=colours_assignment[method],
+    linestyle="-",
+    marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
+    label=latexstring("$(labels_assignment[method]) (\$b_i = \\infty\$)"))
+ax[:plot](xvals, (1/I)*yvals_assignment(string(method), plot_val)[:,1],
+    color=colours_assignment[method],
+    linestyle="--",
+    marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
+    label=latexstring("$(labels_assignment[method]) (\$b_i = 5\$)"))
 ax[:set_xlabel]("Signal-to-noise ratio [dB]")
 ax[:set_ylabel](L"Average number of searches $\eta_i$")
 show_legend!(ax, "upper right")
@@ -136,30 +97,40 @@ fig[:savefig]("$(sim_name)_$(plot_name).eps")
 # instantaneous-sumrate
 plot_name = "instantaneous-sumrate"; plot_val = "weighted_logdet_rates_full"
 fig = PyPlot.figure()
-ax = fig[:add_axes]((0.11,0.15,0.95-0.11,0.95-0.15))
+ax = fig[:add_axes]((0.13,0.15,0.98-0.13,0.96-0.15))
 
-for method in [:BranchAndBoundClustering, :CoalitionFormationClustering_AttachOrSupplant, :Peters2012_Heuristic, :Chen2014_kmeans, :NoClustering, :GrandCoalitionClustering]
+lines = Any[]
+for method in [:BranchAndBoundClustering, :CoalitionFormationClustering_AttachOrSupplant, :Peters2012_Heuristic, :NoClustering, :Chen2014_kmeans, :GrandCoalitionClustering]
     if method == :CoalitionFormationClustering_AttachOrSupplant
-        ax[:plot](xvals, yvals_precoding(string(method), plot_val)[:,2],
+        line1 = ax[:plot](xvals, yvals_precoding(string(method), plot_val)[:,2],
             color=colours_assignment[method],
             linestyle="-",
             marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
             label=latexstring("$(labels_assignment[method]) (\$b_i = \\infty\$)"))
-        ax[:plot](xvals, yvals_precoding(string(method), plot_val)[:,1],
+        line2 = ax[:plot](xvals, yvals_precoding(string(method), plot_val)[:,1],
             color=colours_assignment[method],
             linestyle="--",
             marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
             label=latexstring("$(labels_assignment[method]) (\$b_i = 5\$)"))
+        push!(lines, line1[1])
+        push!(lines, line2[1])
     else
-        ax[:plot](xvals, yvals_precoding(string(method), plot_val)[:,1],
+        line = ax[:plot](xvals, yvals_precoding(string(method), plot_val)[:,1],
             color=colours_assignment[method],
             linestyle=linestyles_assignment[method],
             marker=markers_assignment[method], markeredgecolor=colours_assignment[method], markevery=5,
             label=labels_assignment[method])
+        push!(lines, line[1])
     end
 end
 ax[:set_ylim]([-2, 100])
 ax[:set_xlabel]("Signal-to-noise ratio [dB]")
-ax[:set_ylabel]("Average sum throughput [bits/s/Hz]")
-show_legend!(ax, "upper left")
+ax[:set_ylabel]("WMMSE sum throughput [bits/s/Hz]")
+legend1 = PyPlot.legend(handles=lines[1:3], loc="upper left")
+legend1_frame = legend1[:get_frame]()
+PyPlot.setp(legend1_frame, linewidth=0.5)
+ax[:add_artist](legend1)
+legend2 = ax[:legend](handles=lines[4:7], bbox_to_anchor=[0.5, 0.05], loc="lower left")
+legend2_frame = legend2[:get_frame]()
+PyPlot.setp(legend2_frame, linewidth=0.5)
 fig[:savefig]("$(sim_name)_$(plot_name).eps")
