3bf0a8c6ffbf3f8b915a695458c52d1b7f51d837
Rasmus Brandt <rasmusb85@gmail.com>
2015-09-05 20:26:09 +0200

    SNR batch sim for conference2.

---
+++ batch/conference2/SNR/plot.jl
@@ -0,0 +1,92 @@
+#!/usr/bin/env julia
+
+include("../../../src/IAClustering.jl")
+using IAClustering, CoordinatedPrecoding
+using Compat, JLD, LaTeXStrings
+
+sim_name = "SNR"
+data = load("$(sim_name).jld")
+
+plot_params = [
+    "objective" => :sumrate,
+
+    "methods" => [
+        "BranchAndBoundClustering" => [
+            ("throughputs",),
+        ],
+
+        "CoalitionFormationClustering_AttachOrSupplant" => [
+            ("throughputs",),
+        ],
+
+        "GreedyClustering_Single" => [
+            ("throughputs",),
+        ],
+
+        "GreedyClustering_Multiple" => [
+            ("throughputs",),
+        ],
+
+        "GrandCoalitionClustering" => [
+            ("throughputs",),
+        ],
+
+        "NoClustering" => [
+            ("throughputs",),
+        ],
+    ]
+]
+_, processed_data_mean, _ = postprocess(data["raw_assignment_results"], data["simulation_params"], plot_params)
+idp_vals = data["simulation_params"]["independent_variable"][2]
+
+# 8-class Set1
+colours = [
+    :red => "#e41a1c",
+    :blue => "#377eb8",
+    :green => "#4daf4a",
+    :purple => "#984ea3",
+    :orange => "#ff7f00",
+    :yellow => "#ffff33",
+    :brown => "#a65628",
+    :pink => "#f781bf",
+]
+
+# Plot defaults
+PyPlot.rc("lines", linewidth=1., markersize=3, markeredgewidth=0.5)
+PyPlot.rc("font", size=6, family="serif", serif="Computer Modern Sans Serif")
+PyPlot.rc("text", usetex=true)
+PyPlot.rc("text.latex", preamble="\\usepackage{amsmath}")
+PyPlot.rc("axes", linewidth=0.5, labelsize=6)
+PyPlot.rc("xtick", labelsize=6)
+PyPlot.rc("ytick", labelsize=6)
+PyPlot.rc("legend", fancybox=true, fontsize=6)
+PyPlot.rc("figure", figsize=(3.50,1.5), dpi=125)
+
+# Plot it
+fig = PyPlot.figure()
+ax = fig[:add_axes]((0.12,0.18,0.90-0.12,0.95-0.18))
+
+ax[:plot](idp_vals, processed_data_mean["BranchAndBoundClustering"]["throughputs"],
+    color=colours[:blue], linestyle="-",
+    label="Branch and bound")
+ax[:plot](idp_vals, processed_data_mean["GreedyClustering_Multiple"]["throughputs"],
+    color=colours[:green], linestyle="-",
+    label="Heuristic")
+ax[:plot](idp_vals, processed_data_mean["CoalitionFormationClustering_AttachOrSupplant"]["throughputs"],
+    color=colours[:purple], linestyle="-",
+    label="Coalition formation [x]")
+# ax[:plot](idp_vals, processed_data_mean["GreedyClustering_Single"]["throughputs"],
+#     color=colours[:green], linestyle="--",
+#     label="Heuristic (single)")
+# ax[:plot](idp_vals, processed_data_mean["GrandCoalitionClustering"]["throughputs"],
+#     color=colours[:brown], linestyle="-",
+#     label="Grand coalition")
+ax[:plot](idp_vals, processed_data_mean["NoClustering"]["throughputs"],
+    color=colours[:pink], linestyle="-",
+    label="No clustering")
+ax[:set_xlabel]("Signal-to-noise ratio [dB]")
+ax[:set_ylabel]("Sum throughput [bits/s/Hz]")
+legend = ax[:legend](loc="upper left")
+legend_frame = legend[:get_frame]()
+PyPlot.setp(legend_frame, linewidth=0.5)
+fig[:savefig]("SNR.eps")
---
+++ batch/conference2/SNR/run.jl
@@ -0,0 +1,64 @@
+#!/usr/bin/env julia
+
+include("../../../src/IAClustering.jl")
+using IAClustering, CoordinatedPrecoding
+using Compat, JLD
+
+# Parameters
+include("../simulation_params.jl")
+const Ndrops = 100
+
+simulation_params = [
+    "Ndrops" => Ndrops, "Nsim" => Nsim,
+    "I" => I, "Kc" => Kc,
+    "M" => M, "N" => N, "d" => d,
+    "geography_size" => (geography_width, geography_width),
+    "MS_serving_BS_distance" => MS_serving_BS_distance,
+    "assignment_methods" => [
+        # ExhaustiveSearchClustering,
+        BranchAndBoundClustering,
+
+        CoalitionFormationClustering_AttachOrSupplant,
+
+        GreedyClustering_Single,
+        GreedyClustering_Multiple,
+
+        GrandCoalitionClustering,
+        NoClustering,
+    ],
+    "precoding_methods" => [ NoPrecoding ],
+    "aux_network_params" => [
+        "num_coherence_symbols" => num_coherence_symbols,
+        "beta_network_sdma" => beta_network_sdma,
+    ],
+    "aux_assignment_params" => [
+        "max_num_MSs_per_BS" => Kc,
+
+        "BranchAndBoundClustering:max_abs_optimality_gap" => 0.,
+        "BranchAndBoundClustering:max_rel_optimality_gap" => 0.,
+        "BranchAndBoundClustering:E1_bound_in_rate_bound" => false,
+        "BranchAndBoundClustering:store_fathomed_subtree_sizes" => true,
+    ],
+    "aux_precoding_params" => [
+        "initial_precoders" => "eigendirection",
+        "stop_crit" => 1e-2,
+        "max_iters" => 1000,
+    ],
+    "independent_variable" => (set_average_SNRs_dB!, -10:5:40),
+]
+
+srand(725242)
+network =
+    setup_random_large_scale_network(simulation_params["I"],
+        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
+        num_streams=simulation_params["d"],
+        geography_size=simulation_params["geography_size"],
+        MS_serving_BS_distance=simulation_params["MS_serving_BS_distance"])
+
+_, raw_assignment_results =
+    simulate(network, simulation_params, loop_over=:assignment_methods)
+
+println("-- Saving results")
+save("SNR.jld",
+     "simulation_params", clean_simulation_params_for_jld(simulation_params),
+     "raw_assignment_results", raw_assignment_results)
