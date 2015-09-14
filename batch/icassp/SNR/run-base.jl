ada45c79ea5d3df1f91fe9a95d047a8720765318
Rasmus Brandt <rasmusb85@gmail.com>
2015-09-14 12:20:31 +0200

    Split icassp/SNR into 10 processes.
    - Implement merge later...

--- batch/icassp/SNR/run-base.jl
+++ batch/icassp/SNR/run-base.jl
@@ -6,7 +6,7 @@ using Compat, JLD
 
 # Parameters
 include("../simulation_params.jl")
-const Ndrops = 100
+const Ndrops = 10
 
 simulation_params = [
     "Ndrops" => Ndrops, "Nsim" => Nsim,
@@ -46,19 +46,3 @@ simulation_params = [
     ],
     "independent_variable" => (set_average_SNRs_dB!, -10:5:40),
 ]
-
-srand(725242)
-network =
-    setup_random_large_scale_network(simulation_params["I"],
-        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
-        num_streams=simulation_params["d"],
-        geography_size=simulation_params["geography_size"],
-        MS_serving_BS_distance=simulation_params["MS_serving_BS_distance"])
-
-_, raw_assignment_results =
-    simulate(network, simulation_params, loop_over=:assignment_methods)
-
-println("-- Saving results")
-save("SNR.jld",
-     "simulation_params", clean_simulation_params_for_jld(simulation_params),
-     "raw_assignment_results", raw_assignment_results)
