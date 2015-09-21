978462ac7c9a500ad2f34d38b9dcf07b79540ad9
Rasmus Brandt <rasmusb85@gmail.com>
2015-09-21 17:50:13 +0200

    Remove general_investigation and network_investigation.
    - Not relevant anymore.

--- batch/general_investigation/simulation_params-SNR.jl
+++
@@ -1,6 +0,0 @@
-simulation_params["independent_variable"] = (set_average_SNRs_dB!, -10:10:50)
-simulation_params["aux_independent_variables"] = [
-    ((n, v) -> set_aux_assignment_param!(n, v, "CoalitionFormationClustering:search_budget"), [5, 100_000]),
-    ((n, v) -> set_aux_network_param!(n, v, "num_coherence_symbols"), [num_coherence_symbols, num_coherence_symbols]),
-    ((n, v) -> set_aux_network_param!(n, v, "beta_network_sdma"), [beta_network_sdma, beta_network_sdma]),
-]
--- batch/general_investigation/simulation_params-precoding_methods.jl
+++
@@ -1,9 +0,0 @@
-simulation_params["assignment_methods"] = [ BranchAndBoundClustering, ]
-
-simulation_params["precoding_methods"] = [
-    RobustIntraclusterWMMSE,
-    NaiveIntraclusterWMMSE,
-
-    RobustChen2014_MaxSINR,
-    NaiveChen2014_MaxSINR,
-]
